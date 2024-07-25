2252dafe036d408c1dcb066f9fe1600e4c83cb50
Konstantinos Fragkoulis <konstantinosfragoulis@proton.me>
2024-07-25 13:34:12 +0300

    Improved conversion functions

--- conversions.py
+++ conversions.py
@@ -20,28 +20,38 @@ def ThrustToRPM(thrust: float):
     global const2
     return int((math.sqrt(thrust)) * const2)
 
-const3 = 1/(10196720000**1.011358)
 @safeCall
 def RPMtoThrottleCRSF(rpm: int):
     """Converts the RPM of a motor to a CRSF throttle value [1000, 2000]."""
-    global const3
-    return int(270782500 + (-270781529.4186)/(1 + rpm**1.011358 * const3))
+    if rpm < 0 or rpm > 47000:
+        print(f"\tInvalid value {rpm}. Must be between 0 and 47000.")
+        cleanup()
+        return
+    return int(0.02080581*rpm + 971.4899)
 
 const8 = 65535 / 1000
 const9 = 32768
 @safeCall
 def CRSFtoInt(value: int):
     """Converts from CRSF [1000, 2000] to int16 [-32768, 32767]."""
+    if value > 2000 or value < 1000:
+        print(f"\tInvalid value {value}. Must be between 1000 and 2000.")
+        cleanup()
+        return
     return int(((value - 1000) * const8) - const9)
 
-const4 = 1000 / 65535
-const5 = 1000 + (32768 * const4)
+const3 = 1000 / 65535
+const4 = 1000 + (32768 * const3)
 @safeCall             
 def intToCRSF(value: int):
     """Converts from int16 [-32768, 32767] to CRSF [1000, 2000]."""
-    return int((value * const4) + const5)
+    if value > 32767 or value < -32768:
+        print(f"\tInvalid value {value}. Must be between -32768 and 32767.")
+        cleanup()
+        return
+    return int((value * const3) + const4)
 
-const6 = 360/(32767+32768)
+const5 = 360/(32767+32768)
 @safeCall
 def intToDegPerSec(value: int):
     """Converts Pitch, Roll, and Yaw values [-32768, 32767] to degrees per second."""
@@ -49,9 +59,9 @@ def intToDegPerSec(value: int):
         print(f"\tInvalid value {value}. Must be between -32768 and 32767.")
         cleanup()
         return
-    return const6*value
+    return const5*value
 
-const7 = (32767+32768)/360
+const6 = (32767+32768)/360
 @safeCall
 def degPerSecToInt(value: float):
     """Converts degrees per second to int16 [-32768, 32767]."""
@@ -59,4 +69,4 @@ def degPerSecToInt(value: float):
         print(f"\tInvalid value {value}. Must be between -180 and 180.")
         cleanup()
         return
-    return int(const7 * value)
+    return int(const6 * value)
